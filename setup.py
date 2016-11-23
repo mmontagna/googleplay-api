c61fe5a63ab883948df2a6d4087ed34f0048aed3
Marco Montagna <marcojoemontagna@gmail.com>
2016-11-22 22:38:00 -0800

    Change name.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import sys
 _dir = os.path.dirname(os.path.realpath(__file__))
 
 setup(
-  name = 'google-play-api',
+  name = 'unofficial-googleplay-api',
   packages = find_packages(exclude=["test"]),
-  version = '0.0.1',
+  version = '0.0.2',
   description = '',
   author = 'Marco Montagna',
   author_email = 'marcojoemontagna@gmail.com',
   url = 'https://github.com/mmontagna/googleplay-api',
-  download_url = 'https://github.com/mmontagna/googleplay-api/archive/v0.0.1.tar.gz',
+  download_url = 'https://github.com/mmontagna/googleplay-api/archive/v0.0.2.tar.gz',
   keywords = ['googleplay', 'apk', 'android', 'download'],
   classifiers = [],
   install_requires = ['requests','protobuf'],
